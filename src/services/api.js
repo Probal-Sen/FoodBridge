514def536ba20d78c458e75bdc1a3c75728ba017
Probal Sen <9probalsen@gmail.com>
2025-06-15 00:20:33 +0530

    json updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.NODE_ENV === 'production' 
+  ? '/api'  // In production, use relative path
+  : 'http://localhost:5000/api';  // In development, use local server
 
 // Create axios instance with default config
 const api = axios.create({
